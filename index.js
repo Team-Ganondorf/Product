6a14bd896e1c9a50a296883c2a613582700502f6
Gabriela Olarte <gmolarte@gmail.com>
2022-11-08 18:15:43 -0600

    routes

--- index.js
+++ index.js
@@ -11,69 +11,16 @@ app.use(
   })
 )
 
-app.get('/', (request, response) => {
-  response.json({ info: 'U did it :)' })
-})
-
-
-/*
-*   [
-      {
-        "id": 1,
-        "name":
-        slogan
-        description
-        category
-        default_price
-      },
-      {...}
-    ]
-*/
+app.get('/', (request, response) => { response.json({ info: 'U did it :)' })})
 
 app.get('/products', db.getProducts)
 
-
 app.get('/products/:id/', db.getProductById)
 
+app.get('/products/:id/styles', db.getProductStyle)
 
-app.get('/products/:id/styles', db.getProductById)
-
-
-app.get('/products/:id/related', db.getProductById)
-
-
-
+app.get('/products/:id/related', db.getRelatedProducts)
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
 })
-
-
-
-
-
-// import * as db from './queries.js';
-
-// const express = require('express')
-// const bodyParser = require('body-parser')
-// const app = express()
-// const port = 3000
-
-// app.use(bodyParser.json())
-// app.use(
-//   bodyParser.urlencoded({
-//     extended: true,
-//   })
-// )
-
-// app.get('/', (request, response) => {
-//   response.json({ info: 'Node.js, Express, and Postgres API' })
-// })
-
-// // app.get('/related', db.getRelatedProducts)
-
-
-// app.listen(port, () => {
-//   console.log(`App running on port ${port}.`)
-// })
-
